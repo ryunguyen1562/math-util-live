0524b429c0bc1b3443bf4d4f9fa9e3ed69cb59b3
ryunguyen1562 <phuongduy15620@gmail.com>
2022-08-02 14:20:43 +0700

    Lan thu 2 lam chuyen ay - remember

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -16,6 +16,7 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
+        System.out.println("5! = 120; actual: " + MathUtil.getFactorial(10));
     }
     
 }
